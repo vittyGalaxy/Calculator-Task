07bac5125c0c54fdc98a27f97a5b2d56515dbffd
Vittorione <vittorio.tiozzo.dev@gmail.com>
2024-10-19 20:49:03 +0200

    wip: working on main

---
+++ main.py
@@ -0,0 +1,22 @@
+class calcultor:
+    def __init__(self):
+
+    def sum_number(self):
+        pass
+
+    def sub_number(self):
+        pass
+
+    def mul_number(self):
+        pass
+
+    def div_number(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
