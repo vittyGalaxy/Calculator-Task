af855a8e5a5a5d16cd1231a5227c38ead9be5479
Vittorione <vittorio.tiozzo.dev@gmail.com>
2024-11-07 21:52:35 +0100

    wip: update main

--- Parser.py
+++ Parser.py
@@ -3,3 +3,15 @@
 class Parser:
     def __init__(self):
         pass
+
+    def sum(self):
+        pass
+
+    def sub(self):
+        pass
+
+    def mul(self):
+        pass
+
+    def div(self):
+        pass
