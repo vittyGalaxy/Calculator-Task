4f9d0ac3fc297d8eaee3056a8a8e19fdc36cee6c
Vittorione <vittorio.tiozzo.dev@gmail.com>
2024-11-13 18:59:36 +0100

    wip: update parser

--- Parser.py
+++ Parser.py
@@ -1,17 +1,19 @@
-# Parser class to interpret a simple algebraic expression
+import re
+
 
 class Parser:
     def __init__(self):
-        pass
+        def eval_parentheses(expr):
+            while '(' in expr:
+                expr = re.sub(r'\(([^()]+)\)', lambda x: str(eval_simple(x.group(1))), expr)
+            return eval_simple(expr)
 
-    def sum(self):
-        pass
+        def eval_simple(expr):
+            try:
+                return eval(expr)
+            except Exception as e:
+                raise ValueError(f"Errore durante l'analisi dell'espressione: {e}")
 
-    def sub(self):
-        pass
-
-    def mul(self):
-        pass
-
-    def div(self):
-        pass
+        def parse_expression(expr):
+            expr = expr.replace(" ", "")
+            return eval_parentheses(expr)
